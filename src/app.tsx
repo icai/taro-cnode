89b54d025489b071572144d7c9b8bb1b9462dac4
Terry Cai <gidcai@gmail.com>
2018-09-08 00:22:29 +0800

    适配手机尺寸

--- src/app.tsx
+++ src/app.tsx
@@ -10,6 +10,11 @@ import './assets/scss/iconfont/iconfont.css';
 import './assets/scss/github-markdown.css';
 
 import './app.scss'
+if (process.env.TARO_ENV === "weapp") {
+  require("taro-ui/dist/weapp/css/index.css");
+} else if (process.env.TARO_ENV === "h5") {
+  require("taro-ui/dist/h5/css/index.css");
+}
 
 const store = configStore()
 store.dispatch(actions.authCheckState());
