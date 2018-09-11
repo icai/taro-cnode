3e67248044992768ad2e2e21ee85bc60f44e41e0
Terry Cai <gidcai@gmail.com>
2018-09-11 15:13:51 +0800

    皮皮虾，换行啊

--- src/components/reply/index.tsx
+++ src/components/reply/index.tsx
@@ -34,7 +34,7 @@ class Reply extends Component<Iprops, PageState> {
     hasErr: false,
     content: "",
     author_txt:
-      "\n 来自拉风的 [Taro-cnode](https://github.com/icai/taro-cnode)"
+      "\n\n 来自拉风的 [Taro-cnode](https://github.com/icai/taro-cnode)"
   };
 
   componentWillReceiveProps(nextProps) {
--- src/pages/add/index.tsx
+++ src/pages/add/index.tsx
@@ -23,19 +23,23 @@ class Add extends Component<{}, {}> {
       content: ""
     },
     selectorIndex: 0,
-    selector: [{
-      name: '分享',
-      value: 'share'
-    }, {
-        name: '问答',
-        value: 'ask'
-    }, {
-        name: '招聘',
-        value: 'job'
-    }],
+    selector: [
+      {
+        name: "分享",
+        value: "share"
+      },
+      {
+        name: "问答",
+        value: "ask"
+      },
+      {
+        name: "招聘",
+        value: "job"
+      }
+    ],
     err: "",
     authorTxt:
-      "\n 来自拉风的 [Taro-cnode](https://github.com/icai/taro-cnode)"
+      "\n\n 来自拉风的 [Taro-cnode](https://github.com/icai/taro-cnode)"
   };
   addTopic() {
     let title = utils.trim(this.state.topic.title);
@@ -101,37 +105,65 @@ class Add extends Component<{}, {}> {
   };
   render() {
     const { err, selectorIndex } = this.state;
-    return <View className="flex-wrp">
+    return (
+      <View className="flex-wrp">
         <Header pageType={"主题"} fixHead={true} showMenu={true} />
         <View className="add-container">
           <View className="line">
             选择分类：
-            <Picker className="add-tab" mode="selector" value={selectorIndex} range={this.state.selector} rangeKey={"name"} onChange={this.handleTopicTabChange}>
-              <View className="picker">{this.state.selector[selectorIndex]['name']}</View>
+            <Picker
+              className="add-tab"
+              mode="selector"
+              value={selectorIndex}
+              range={this.state.selector}
+              rangeKey={"name"}
+              onChange={this.handleTopicTabChange}
+            >
+              <View className="picker">
+                {this.state.selector[selectorIndex]["name"]}
+              </View>
             </Picker>
             {/* <select className="add-tab" value={this.state.topic.tab} onChange={this.handleTopicTabChange}>
               <option value="share">分享</option>
               <option value="ask">问答</option>
               <option value="job">招聘</option>
             </select> */}
-            <View className="add-btn" onClick={e => {
+            <View
+              className="add-btn"
+              onClick={e => {
                 this.addTopic();
-              }}>
+              }}
+            >
               发布
             </View>
           </View>
           <View className="line">
-            <AtInput className={classNames({
+            <AtInput
+              className={classNames({
                 "add-title": 1,
                 err: err == "title"
-              })} value={this.state.topic.title} onChange={this.handleTopicChange} type="text" placeholder="标题，字数10字以上" max-length="100" />
+              })}
+              value={this.state.topic.title}
+              onChange={this.handleTopicChange}
+              type="text"
+              placeholder="标题，字数10字以上"
+              max-length="100"
+            />
           </View>
-          <AtTextarea className={classNames({
+          <AtTextarea
+            className={classNames({
               "add-content": 1,
               err: err == "content"
-            })} value={this.state.topic.content} onChange={this.handleTopicContentChange} maxlength={9999} height="400" placeholder="回复支持Markdown语法,请注意标记代码" />
+            })}
+            value={this.state.topic.content}
+            onChange={this.handleTopicContentChange}
+            maxlength={9999}
+            height="400"
+            placeholder="回复支持Markdown语法,请注意标记代码"
+          />
         </View>
-      </View>;
+      </View>
+    );
   }
 }
 
